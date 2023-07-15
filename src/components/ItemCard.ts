b7bc66fb617c40e925be6c021bd9519cdd1af646
Selman Ciftci <selman.ciftci.coding@gmail.com>
2023-07-15 12:56:20 +0200

    refactored ItemCard component

--- src/components/ItemCard.ts
+++ src/components/ItemCard.ts
@@ -1,18 +1,18 @@
 import { IItemCard } from "../interfaces";
 
-export const ItemCard = (icon: IItemCard) => {
+export const ItemCard = (item: IItemCard) => {
   return /*html*/ `
  <div class="card w-96 bg-neutral shadow-xl mt-3">
     <figure class="bg-white">
-      <a href="${icon.url}"><img src="${icon.image}" alt="preview" /> </a>
+      <a href="${item.url}"><img src="../../public/img/${item.name}.png" alt="preview" /> </a>
     </figure>
     <div class="card-body">
       <h2 class="card-title">
-        ${icon.name}
-        <div class="badge badge-primary">${icon.badge}</div>
+        ${item.name}
+        <div class="badge badge-primary">${item.badge}</div>
       </h2>
       <p>
-        ${icon.description}
+        ${item.description}
       </p>
       <div class="card-actions justify-end"></div>
     </div>
